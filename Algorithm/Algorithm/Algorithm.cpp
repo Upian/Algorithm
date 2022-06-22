6b767e3fc40ab42a9391d2d929640cd4920c1a0c
Upian <terry0729@naver.com>
2022-06-23 01:17:27 +0900

    Initial commit

---
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -0,0 +1,13 @@
+﻿// Algorithm.cpp : 애플리케이션의 진입점을 정의합니다.
+//
+
+#include "Algorithm.h"
+#include "FindPath/BFS.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
